1c6db2a6a8003877a60b934dbb40f9d3b62a3343
Jonathan Devere-Ellery <jonathan@devere-ellery.com>
2023-12-06 18:07:55 +0100

    Tweaks

--- .github/build/build.ps1
+++ .github/build/build.ps1
@@ -11,3 +11,7 @@ Import-PackageProvider -Name NuGet -force | Out-Null
 if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
+Write-Verbose ""
+Update-ModuleManifest -Path ".\MyTestSOAModule\MyTestSOAModule.psd1" -ModuleVersion ($env:MODULE_VER).TrimStart("v")
+Write-Host ${{ github.event.pull_request.head.ref }}
+git branch --show-current
